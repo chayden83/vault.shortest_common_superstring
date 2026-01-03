3e4cfe24b367db685fd0cdd7611feb9e993c983d
Chris Hayden <christopher.maurice.hayden@gmail.com>
2026-01-03 22:29:50 +0000

    Record current benchmark results.

--- benchmarks/benchmarks.shortest_common_superstring.cpp
+++ benchmarks/benchmarks.shortest_common_superstring.cpp
@@ -16,6 +16,15 @@
 
 // clang-format off
 
+// ---------------------------------------------------------------------------
+// Benchmark                                 Time             CPU   Iterations
+// ---------------------------------------------------------------------------
+// shortest_common_superstring/256    38510231 ns     38496430 ns           17
+// shortest_common_superstring/512   210588417 ns    210581596 ns            3
+// shortest_common_superstring/1024 1029923903 ns   1029466393 ns            1
+// shortest_common_superstring/2048 3990672851 ns   3988920320 ns            1
+// shortest_common_superstring/4096 1.4399e+10 ns   1.4383e+10 ns            1
+// shortest_common_superstring/8192 4.1446e+10 ns   4.1427e+10 ns            1
 void shortest_common_superstring(benchmark::State &state) {
   auto strings = vault::internal::random_words_10k()
     | ::ranges::views::transform([](auto arg) { return std::string { arg }; })
